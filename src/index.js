3b20a2e1fa408fef2dc1528897605b465ea4cd34
Victor Ingman <viregistermail@gmail.com>
2021-03-28 21:47:47 +0200

    :mag: Improve SEO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <Route path='/:page?'>
